4a0cde56d04216737f799fe014f1a218a049fd56
Marvin Hamacher <Xavahus@gmail.com>
2024-09-15 18:21:56 +0200

    alter date

--- screenshots/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from screenshots.models import Screenshot
-
-
-class ScreenshotSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Screenshot
-        fields = '__all__'
